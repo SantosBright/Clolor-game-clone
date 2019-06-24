47799debb3a9c3ecb17ef696caad3dd289bdc0b1
SantosBright <itzsantosbright15@gmail.com>
2019-06-24 05:06:57 +0300

    add message display login and sytle it

--- script.js
+++ script.js
@@ -13,13 +13,17 @@ var colorDisplay = document.querySelector("#colorDisplay");
 colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++){
+    var messageDisplay = document.querySelector("#messageDisplay");
+
     squares[i].style.background = colors[i];
     squares[i].addEventListener("click", function(){
         var clickedColor = this.style.background;
         if(clickedColor === pickedColor){
             changeColor(clickedColor);
+            messageDisplay.textContent = "Correct!";
         } else {
             this.style.background = "#232323";
+            messageDisplay.textContent = "Wrong!"
         }
     });
 } 
