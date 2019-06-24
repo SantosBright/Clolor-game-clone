b3718fe56670b37cba1aac888915c0134d190fdf
SantosBright <itzsantosbright15@gmail.com>
2019-06-24 04:54:20 +0300

    add click login and checking if the clicked color is equal to the assigned color

--- script.js
+++ script.js
@@ -7,7 +7,23 @@ var colors = [
     'rgb(255, 255, 0)'
 ]
 var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+
 
 for(var i = 0; i < squares.length; i++){
     squares[i].style.background = colors[i];
+    squares[i].addEventListener("click", function(){
+        var clickedColor = this.style.background;
+        if(clickedColor === pickedColor){
+            changeColor(clickedColor)
+        } else {
+            this.style.background = "#232323";
+        }
+    });
 } 
+
+function changeColor(color){
+    for(var i = 0; i < squares.length; i++){
+        squares[i].style.background = color;
+    }
+}
