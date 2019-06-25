42f82b0e57214f79091aeaaa5016175aa5a1e551
SantosBright <itzsantosbright15@gmail.com>
2019-06-25 07:43:05 +0300

    add more styles snd glitter to the page

--- script.js
+++ script.js
@@ -62,8 +62,9 @@ resetBtn.addEventListener("click", function(){
     for(var i =0; i < squares.length; i++){
         squares[i].style.background = colors[i];
     }
-    h1.style.background = "#232323";
+    h1.style.background = "steelblue";
     resetBtn.textContent = "New Colors"
+    messageDisplay.textContent = " "
 });
 
 function changeColor(color){
