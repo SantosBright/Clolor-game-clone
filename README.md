ee5856f95fe569a0eb26542af0eb20ec7d95531b
SantosBright <itzsantosbright15@gmail.com>
2019-06-25 05:28:08 +0300

    update colors to generate random colors

--- README.md
+++ README.md
@@ -16,13 +16,36 @@ inside the loop add an eventlistener to the squares
 grab the color of the clicked square
 compare the color to pickedColor
 if yes, display "correct" on the page and
-turn all the squares the same as the clicked square
+turn all the squares the same color as the clicked square
 if no, turn the clicked square the same color of the page and
 display "wrong" on the page
 
 pt.2
 update pickedColor to pick random colors
+after comparing, if yes, then, turn h1 the same color as clickedColor
+
 
 pt.3
 update colors to the generate random colors
+    repeat num times
+    create an empty array
+        get random color and push into the array
+    return that array
 
+pt.4
+give the div below the h1 an id 
+style the div
+add a button inside the div
+add an click listener to the button
+    generate all new colors
+    pick a new random color from the array
+    change display color to match pick color
+    change colors of squares on the page
+    reset the h1 back to it initial color
+    when user wins, update the text in button to "Play again"
+
+
+pt.5
+add hard and easy button to impact on the game
+define a class
+    background blue
--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-var colors = [
-    'rgb(255, 0, 255)',
-    'rgb(255, 0, 0)',
-    'rgb(0, 0, 255)',
-    'rgb(0, 255, 255)',
-    'rgb(0, 255, 0)',
-    'rgb(255, 255, 0)'
-]
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.querySelector("#colorDisplay");
@@ -14,6 +7,7 @@ colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++){
     var messageDisplay = document.querySelector("#messageDisplay");
+    var h1 = document.querySelector("h1");
 
     squares[i].style.background = colors[i];
     squares[i].addEventListener("click", function(){
@@ -21,6 +15,7 @@ for(var i = 0; i < squares.length; i++){
         if(clickedColor === pickedColor){
             changeColor(clickedColor);
             messageDisplay.textContent = "Correct!";
+            h1.style.background = clickedColor;
         } else {
             this.style.background = "#232323";
             messageDisplay.textContent = "Wrong!"
@@ -36,4 +31,17 @@ function changeColor(color){
 function pickColor(num){
     var random = Math.floor(Math.random() * colors.length);
     return colors[random];
+}
+function generateRandomColors(num){
+    var arr = [];
+    for(var i =0; i < num; i++){
+        arr.push(randomColors());
+    }
+    return arr;
+}
+function randomColors(){
+   var r =  Math.floor(Math.random() * 256 );
+   var g =  Math.floor(Math.random() * 256 );
+   var b =  Math.floor(Math.random() * 256 );
+   return "rgb(" + r + ", " + g + ", " + b + ")";
 }
